3c6cb6028380965a89d50bdbf4715019fde2e0f7
Evan Mac Hale <evan.machale@shipyardtech.com>
2021-02-11 09:20:12 +0000

    section-15

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,98 @@
 <template>
-  <the-navigation></the-navigation>
-  <main>
-    <router-view></router-view>
-  </main>
-  <footer>
-    <router-view name="footer"></router-view>
-  </footer>
+  <the-header></the-header>
+  <router-view></router-view>
 </template>
 
 <script>
-import TheNavigation from './components/nav/TheNavigation.vue';
+import TheHeader from './components/nav/TheHeader.vue';
 
 export default {
   components: {
-    TheNavigation
+    TheHeader
   },
   data() {
     return {
-      teams: [
-        { id: 't1', name: 'Frontend Engineers', members: ['u1', 'u2'] },
-        { id: 't2', name: 'Backend Engineers', members: ['u1', 'u2', 'u3'] },
-        { id: 't3', name: 'Client Consulting', members: ['u4', 'u5'] },
+      isLoggedIn: false,
+      products: [
+        {
+          id: 'p1',
+          image:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Books_HD_%288314929977%29.jpg/640px-Books_HD_%288314929977%29.jpg',
+          title: 'Book Collection',
+          description:
+            'A collection of must-read books. All-time classics included!',
+          price: 99.99,
+        },
+        {
+          id: 'p2',
+          image:
+            'https://upload.wikimedia.org/wikipedia/en/thumb/c/c9/Tent_at_High_Shelf_Camp_cropped.jpg/640px-Tent_at_High_Shelf_Camp_cropped.jpg',
+          title: 'Mountain Tent',
+          description: 'A tent for the ambitious outdoor tourist.',
+          price: 129.99,
+        },
+        {
+          id: 'p3',
+          image:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg/640px-Good_Food_Display_-_NCI_Visuals_Online.jpg',
+          title: 'Food Box',
+          description:
+            'May be partially expired when it arrives but at least it is cheap!',
+          price: 6.99,
+        },
       ],
-      users: [
-        { id: 'u1', fullName: 'Max Schwarz', role: 'Engineer' },
-        { id: 'u2', fullName: 'Praveen Kumar', role: 'Engineer' },
-        { id: 'u3', fullName: 'Julie Jones', role: 'Engineer' },
-        { id: 'u4', fullName: 'Alex Blackfield', role: 'Consultant' },
-        { id: 'u5', fullName: 'Marie Smith', role: 'Consultant' },
-      ],
+      cart: { items: [], total: 0, qty: 0 },
     };
   },
   provide() {
     return {
-      teams: this.teams,
-      users: this.users,
+      isLoggedIn: this.isLoggedIn,
+      products: this.products,
+      cart: this.cart,
+      addProductToCart: this.addProductToCart,
+      removeProductFromCart: this.removeProductFromCart,
+      login: this.login,
+      logout: this.logout,
     };
-  }
+  },
+  methods: {
+    addProductToCart(productData) {
+      const productInCartIndex = this.cart.items.findIndex(
+        (ci) => ci.productId === productData.id
+      );
+
+      if (productInCartIndex >= 0) {
+        this.cart.items[productInCartIndex].qty++;
+      } else {
+        const newItem = {
+          productId: productData.id,
+          title: productData.title,
+          image: productData.image,
+          price: productData.price,
+          qty: 1,
+        };
+        this.cart.items.push(newItem);
+      }
+      this.cart.qty++;
+      this.cart.total += productData.price;
+    },
+
+    removeProductFromCart(prodId) {
+      const productInCartIndex = this.cart.items.findIndex(
+        (cartItem) => cartItem.productId === prodId
+      );
+      const prodData = this.cart.items[productInCartIndex];
+      this.cart.items.splice(productInCartIndex, 1);
+      this.cart.qty -= prodData.qty;
+      this.cart.total -= prodData.price * prodData.qty;
+    },
+    login() {
+      this.isLoggedIn = true;
+    },
+    logout() {
+      this.isLoggedIn = false;
+    },
+  },
 };
 </script>
 
